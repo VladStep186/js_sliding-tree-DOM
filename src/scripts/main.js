c83636babb05f265a0de36166a4121136735e666
Vladyslav Stepanenko <vlad.step.186@gmail.com>
2025-07-25 11:39:32 +0300

    created hidden list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+// const li = document.querySelectorAll('li');
+
+// for ()
+// if (!li.querySelector('ul')) {
+//   return;
+// }
+const tree = document.querySelector('.tree');
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+  const text = li.firstChild;
+
+  if (!text || text.nodeType !== 3) {
+    continue;
+  }
+
+  span.textContent = text.textContent;
+  li.replaceChild(span, text);
+}
+
+tree.onclick = function (evente) {
+  const li = evente.target.closest('li');
+  const ul = li.querySelector('ul');
+
+  if (evente.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  if (ul.style.display === 'none') {
+    ul.style.display = '';
+  } else {
+    ul.style.display = 'none';
+  }
+};
